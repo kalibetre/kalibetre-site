1cecec19e0080ba69f1c96db342aebd08817cc1c
kalibetre <kalbetre@icloud.com>
2023-07-03 23:38:59 +0300

    increase project title and subtitle font sizes

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,13 +16,13 @@ const ProjectCard: React.FC<ProjectCardProps> = (props) => {
         <Link to={`/projects/${slug}`}>
             <div
                 key={slug}
-                className="group flex w-full flex-col justify-between rounded-md p-2 shadow-teal-400 hover:shadow-lg dark:hover:shadow-slate-500"
+                className="group flex w-full flex-col justify-between rounded-md p-4 shadow-teal-400 hover:shadow-lg dark:hover:shadow-slate-500"
             >
                 <div className="mb-4">
-                    <h2 className="text-md line-clamp-2 group-hover:text-teal-600">
+                    <h2 className="text-lg line-clamp-2 group-hover:text-teal-600">
                         {title}
                     </h2>
-                    <h3 className="text-xs text-slate-400 line-clamp-2">
+                    <h3 className="text-sm text-slate-400 line-clamp-2">
                         {desc}
                     </h3>
                 </div>
