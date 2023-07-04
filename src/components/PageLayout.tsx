3ca8cc029cda978d0a3bd2dfe1f0a76c38c4a96e
kalibetre <kalbetre@icloud.com>
2023-07-04 11:27:35 +0300

    add a subtle background to the page

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -8,12 +8,15 @@ type PageLayoutProps = {
 const PageLayout: React.FC<PageLayoutProps> = (props) => {
     const { children } = props;
     return (
-        <div className="mx-4 h-screen md:mx-auto md:w-5/6 md:max-w-screen-lg">
-            <PageHeader />
-            <main>{children}</main>
-            <footer className="mt-24 py-8 text-center text-xs">
-                Kalkidan Betre &copy; 2023
-            </footer>
+        <div className="relative">
+            <div className="custom-bg dark:custom-bg-drk pointer-events-none absolute inset-x-0 top-0 h-[350px] w-full opacity-30"></div>
+            <div className="mx-4 h-screen md:mx-auto md:w-5/6 md:max-w-screen-lg">
+                <PageHeader />
+                <main>{children}</main>
+                <footer className="mt-24 py-8 text-center text-xs">
+                    Kalkidan Betre &copy; 2023
+                </footer>
+            </div>
         </div>
     );
 };
