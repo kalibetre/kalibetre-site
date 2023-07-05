30a3e5e909876d0642b8deb646f23dbc9973ad80
kalibetre <kalbetre@icloud.com>
2023-07-05 22:39:21 +0300

    svg fix tags

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -11,23 +11,23 @@ const CloseIcon = () => {
             <path
                 d="M9.16998 14.83L14.83 9.17004"
                 className="stroke-slate-400 dark:stroke-slate-400"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
             <path
                 d="M14.83 14.83L9.16998 9.17004"
                 className="stroke-slate-400 dark:stroke-slate-400"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
             <path
                 d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
                 className="stroke-slate-400 dark:stroke-slate-400"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
         </svg>
     );
