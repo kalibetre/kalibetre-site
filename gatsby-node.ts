f3823429cc97f4f5ffb8a872c62615a4cba3b304
kalibetre <kalbetre@icloud.com>
2023-07-05 22:58:50 +0300

    remove debug console

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,7 +3,6 @@ import { createFilePath } from 'gatsby-source-filesystem';
 export const onCreateNode = ({ node, getNode, actions }: any) => {
     const { createNodeField } = actions;
     if (node.internal.type === `Mdx`) {
-        console.log(node.frontmatter.type);
         const value = createFilePath({ node, getNode }).replace(/^\/|\/$/g, '');
         createNodeField({
             name: `slug`,
