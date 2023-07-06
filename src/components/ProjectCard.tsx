7dda35913479e433448cdd9d3c9a39bec6c769b5
kalibetre <kalbetre@icloud.com>
2023-07-06 23:42:07 +0300

    query data from yaml instead of mdx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,35 +1,38 @@
-import { Link } from 'gatsby';
-import React from 'react';
-import GatsbyImageWrapper from './GatsbyImageWrapper';
+import { Link } from "gatsby";
+import React from "react";
+import GatsbyImageWrapper from "./GatsbyImageWrapper";
 
 type ProjectCardProps = {
-    slug: string;
-    title: string;
-    desc: string;
-    image: string;
+  date: string;
+  title: string;
+  desc: string;
+  blogUrl: string;
+  url: string;
+  image: string;
 };
 
 const ProjectCard: React.FC<ProjectCardProps> = (props) => {
-    const { slug, title, desc, image } = props;
+  const { date, title, desc, blogUrl, url, image } = props;
 
-    return (
-        <Link to={slug}>
-            <div
-                key={slug}
-                className="group flex w-full flex-col justify-between rounded-md p-4 shadow-teal-400 hover:shadow-lg dark:hover:shadow-slate-500"
-            >
-                <div className="mb-4">
-                    <h2 className="text-lg line-clamp-2 group-hover:text-teal-600">
-                        {title}
-                    </h2>
-                    <h3 className="text-sm text-slate-400 line-clamp-2">
-                        {desc}
-                    </h3>
-                </div>
-                <GatsbyImageWrapper image={image} alt={title} />
-            </div>
-        </Link>
-    );
+  const content = () => (
+    <div className="group flex w-full flex-col justify-between rounded-md p-4 shadow-teal-400 hover:shadow-lg dark:hover:shadow-slate-500">
+      <div className="mb-4">
+        <h2 className="line-clamp-2 text-lg group-hover:text-teal-600">
+          {title}
+        </h2>
+        <h3 className="line-clamp-2 text-sm text-slate-400">{desc}</h3>
+      </div>
+      <GatsbyImageWrapper image={image} alt={title} />
+    </div>
+  );
+
+  return blogUrl ? (
+    <Link to={blogUrl}>{content()}</Link>
+  ) : (
+    <a href={url} target="_blank">
+      {content()}
+    </a>
+  );
 };
 
 export default ProjectCard;
